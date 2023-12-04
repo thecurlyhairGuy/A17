56b4c101ac9865651309e0538a5cb04b99a32797
Curly hair boy <149983691+thecurlyhairGuy@users.noreply.github.com>
2023-12-04 04:06:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 
 //
 global.available = true;
-global.autoReadAll = false;
+global.autoReadAll = true;
 global.antitags = true;
 
 
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["239132095836", "2347086606304"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["2349132095836", "2347086606304"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["2349132095836];
+global.OwnerName = "THE CURLYHAIR GUY";
+global.BotName = "THE CURLY HAIRGUY";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
