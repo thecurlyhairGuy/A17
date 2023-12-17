74bfb93ff9a29642d7e012d6e4a62c251513610b
Curly hair boy <149983691+thecurlyhairGuy@users.noreply.github.com>
2023-12-16 17:28:38 -0800

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ global.antitags = true;
 
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = false;                //auto recording by default off.
-global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.autoRecord = true;                //auto recording by default off.
+global.groupevent = true;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = true;
 
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["239132095836", "2347086606304"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["2349132095836", "2347086606304"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["2349132095836];
+global.Owner = ["2348121094113", "2347086606304"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["2348121094113", "2347086606304"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["2348121094113];
 global.OwnerName = "THE CURLYHAIR GUY";
-global.BotName = "THE CURLY HAIRGUY";
+global.BotName = "THE CURLY HAIRGUY💎";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
